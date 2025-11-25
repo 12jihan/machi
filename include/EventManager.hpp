be7e59c6db3d8c1fc01fc03965544e738fac84bd
12jihan <jareem.e.hoff@gmail.com>
2025-11-24 19:53:51 -0500

    making more changes to the event system to make it a bit easier to manage -- making an EventManager class

---
+++ include/EventManager.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <string>
+
+enum class EventType {
+  WindowResize,
+  WindowClose,
+  KeyPress,
+  KeyRelease,
+  MousePress,
+  MouseRelease,
+  MouseMove,
+  MouseScroll,
+  EngineShutdown
+};
+
+struct Event {
+  EventType type;
+  std::string eventName;
+
+  // Union to hold different event data efficiently
+  union {
+    struct {
+      int width, height;
+    } resize;
+    struct {
+      int key, scancode, mods;
+    } keyboard;
+    struct {
+      int button, mods;
+    } mouse;
+    struct {
+      double x, y;
+    } mousePos;
+    struct {
+      double xOffset, yOffset;
+    } scroll;
+  } data;
+
+  double timestamp;  // When the event occurred
+};
+
+// Defining the Callback Type
+using EventHandler = std::function<void(const Event&)>;
+
+class EventManager {
+private:
+  // List of subscribers & Queue of events waiting to be processed:
+  std::vector<EventHandler> m_subscribers;
+  std::vector<Event> m_eventQueue;
+
+public:
+  EventManager() = default;
+  ~EventManager() = default;
+
+  // Subscibe a function to listen for events
+  void subscribe();
+
+  // Add an event to the queue (to be processed next frame)
+  void postEvent();
+
+  // Dispatch all queued events to subscribers immediately
+  void dispatchEvents();
+
+  // Utility: Clear all events without processing (good for resets)
+  void clearQueue();
+};
---
+++ src/EventManager.cpp
@@ -0,0 +1,6 @@
+#include "../include/EventManager.hpp"
+
+void EventManager::subscribe() {}
+void EventManager::postEvent() {}
+void EventManager::dispatchEvents() {}
+void EventManager::clearQueue() {}
